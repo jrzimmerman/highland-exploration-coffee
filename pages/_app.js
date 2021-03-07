108c19758291fbee8bd44bf68380c801e15a255e
Justin Zimmerman <zimmy130@gmail.com>
2021-03-07 15:09:04 -0500

    add tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import '../styles/globals.css'
+import "../src/css/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
